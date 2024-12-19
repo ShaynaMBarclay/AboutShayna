119d850679ef985d9c6b556d70fe2fc66ef6bbce
ShaynaMBarclay <shaynambarclay@gmail.com>
2024-12-19 16:37:57 +0100

    my first commit

---
+++ aboutshayna/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>This is Shayna's resume</h1>
+       
+    </>
+  )
+}
+
+export default App
