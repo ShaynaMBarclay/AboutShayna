8ec436fbf88c01d5bbabc69abb2ace5d1931e5fc
ShaynaMBarclay <shaynambarclay@gmail.com>
2024-12-20 16:33:22 +0100

    another

--- aboutshayna/src/App.jsx
+++ aboutshayna/src/App.jsx
@@ -1,11 +1,37 @@
-import { useState } from 'react'
-import './App.css'
+//import { useState } from 'react'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import AboutMe from "./assets/pages/AboutMe";
+import Blog from "./assets/pages/Blog";
+import Contact from "./assets/pages/contact";
+import HomePage from "./assets/pages/HomePage";
+import Projects from "./assets/pages/Projects";
+import Skills from "./assets/pages/Skills";
+import Footer from "./assets/components/Footer";
+import Navbar from "./assets/components/Navbar";
+import SkillBadge from "./assets/components/SkillBadge";
+//import { useEffect, useState } from "react";
+//import axios from "axios";
 
 function App() {
+
   
   return (
     <>
       <h1>This is Shayna's resume</h1>
+      <main>
+        <Navbar classname="navbar" />
+        <SkillBadge classname="skillbadge" />
+        <Routes>
+          <Route path= "/" element={<HomePage />} />
+          <Route path= "/about-me" element={<AboutMe />} />
+          <Route path= "/blog" element={<Blog />} />
+          <Route path= "/contact" element={<Contact />} />
+          <Route path = "/projects" element={<Projects />} />
+          <Route path = "/skills" element={<Skills />} />
+        </Routes>
+      </main>
+      <Footer />
        
     </>
   )
---
+++ aboutshayna/src/assets/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+const HomePage = () => {
+
+
+
+return (
+    <div className="homepage">
+        <h1> Shaynas Resume </h1>
+    </div>
+)
+}
+
+export default HomePage;
