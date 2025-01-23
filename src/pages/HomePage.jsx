dbeac78042dca261bcb41a17774c2ac7cc796a6e
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-01-23 18:06:09 +0100

    changed homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
+import pfp from '../assets/pfpblacknwhite.jpg';
 
 function HomePage() {
     return (
         <div className="home-page">
             <section className="hero">
                 <h1>Hello! I'm Shayna!👋</h1>
+              
+              <section className="middle-hero">
+                <img src={pfp} alt="Shayna's Profile" className="profile-picture" />
+
                 <h2> I am a Full-Stack Web Developer with a love for creativity and innovation.</h2>
-                <p>I'm a passionate Full-Stack Web Developer specializing in creating responsive and user-friendly web applications.
+                </section>
+
+                {/*<p>I'm a passionate Full-Stack Web Developer specializing in creating responsive and user-friendly web applications.
                    I enjoy turning ideas into interactive digital experiences, with strong communication and customer support. Let's build something amazing together!
                    </p>
-                   <p>
-                   <Link to="/about-me" className="see-more-link"> Read More→</Link> 
-                </p>
+                   <p> */}
+                   <Link to="/about-me" className="see-more-link"> About Me</Link> 
+               
             </section>
             
         </div>
