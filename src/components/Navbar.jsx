8f882626da5ce4e343bcd387d9596584854dadc1
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-01-23 17:03:48 +0100

    changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,53 @@
-import { Link } from "react-router-dom";
-import pfp from '/src/assets/pfp.jpg';
+import { Link, NavLink } from "react-router-dom";
 import { FaHome, FaUser, FaEnvelope, FaProjectDiagram, FaTools } from "react-icons/fa";
 
 function Navbar() {
     
     return (
-        <nav className= "navbar">
-            {/* Profile Picture*/}
-            <div className="navbar-logo">
-                <Link to="/">
-                <img src={pfp} alt="Profile" />
-                </Link>
-                </div>
-
+        <nav className= "vertical-navbar">
             <ul className= "navbar-links">
-                
-                
-                    <Link to="/"><FaHome />Home</Link>
-                
-                    <Link to="about-me"><FaUser />About Me</Link>
-                    
-                    <Link to="/skills"><FaTools />Skills</Link>
-                
-                    <Link to="/contact"><FaEnvelope />Contact</Link>
-                
-                    <Link to="/projects"><FaProjectDiagram />Projects</Link>
-                
-                    
-                
-            </ul>
-        </nav>
+              <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "active-link" : undefined
+            }  
+            >
+               <FaHome />
+               </NavLink>
+               <NavLink
+               to="/about-me"
+               className={({ isActive }) =>
+                  isActive ? "active-link" : undefined
+            }
+          >
+            <FaUser />
+          </NavLink>
+          <NavLink
+            to="/skills"
+            className={({ isActive }) =>
+            isActive ? "active-link" : undefined
+            }
+            >
+                <FaTools />
+            </NavLink>
+            <NavLink 
+            to="/contact"
+            className={({ isActive }) =>
+              isActive ? "active-link" : undefined
+            }
+            >
+             <FaEnvelope />
+            </NavLink>
+          <NavLink
+            to="/projects"
+            className={({ isActive }) =>
+            isActive ? "active-link" : undefined
+        }
+        >
+           <FaProjectDiagram />
+        </NavLink>
+        </ul>
+       </nav>
     );
 }
 
