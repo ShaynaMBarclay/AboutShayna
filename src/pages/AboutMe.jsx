479541cee8528c803ae072b25199488d99ccf5bc
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-01-28 14:48:51 +0100

    fixed about me

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -3,10 +3,12 @@ function AboutMe() {
     return (
         <div>
             <section className="about-me">
-             <p>Hello! I'm a Full-Stack Web Developer based in the United States, passionate about turning ideas into dynamic, real-world solutions. My creative background has shaped my ability to approach coding with an intuitive sense of design, balance, and innovation. I earned a certificate from Ironhack France, excelling in Frontend and Backend development while mastering remote collaboration across time zones. My love for travel and diverse cultures has also taught me to embrace flexibility and teamwork. Let’s work together to bring your vision to life!</p>  
-             <hr className="divider" />
+                <div className="about-text">
+             <p>Hello! I'm a Full-Stack Web Developer based in the United States, passionate about turning ideas into dynamic, real-world solutions. My creative background has shaped my ability to approach coding with an intuitive sense of design, balance, and innovation. I earned a certificate from Ironhack France, excelling in Frontend and Backend development while mastering remote collaboration across time zones. My love for travel and diverse cultures has also taught me to embrace flexibility and teamwork. Let’s work together to bring your vision to life!
+                </p>  
+                </div>
+                <div className="divider"></div>
              <div className="skills-section">
-                    <h3>My Skills</h3>
                     <div className="skills-boxes">
                         <div className="skill-box">
                         <FaLightbulb className="skill-icon" />
@@ -22,8 +24,8 @@ function AboutMe() {
                             </div>
                         <div className="skill-box">
                         <FaRocket className="skill-icon" />
-                            Dedicated & Motivated
-                            </div>
+                            Dedicated
+                       </div>
                     </div>
                 </div>
             </section>
