bcfce2078c54c7507b631f6db997a21f76f9abe1
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-03-03 13:39:24 +0100

    added projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,6 +3,8 @@ import ProjectModal from "../components/ProjectModal";
 import DomofSylv from "../assets/DomofSylv.png";
 import adoptme from "../assets/adoptme.png";
 import portfolio from "../assets/portfolio.png";
+import pixelpet from "../assets/pixelpet.png";
+import frames from "../assets/Frames.png";
 
 function Projects() {
 
@@ -36,6 +38,26 @@ function Projects() {
             Built with React and functional components, this site is a continuous work in progress as I refine my abilities. 
             Thanks for stopping by! Feel free to reach out if you have any questions or want to connect.`,
             link: "https://aboutshayna.netlify.app/"
+        },
+        {
+            id: 4,
+            title: "Pixel Pet (WIP)",
+            image: pixelpet,
+            description: `Pixel Pet is a React-based browser game inspired by early 2000s Tamagotchi pets. 
+            Players feed, pet, and bathe their pixel pet to keep it happy and healthy while managing its hunger, happiness, and health bars before they drop too low.
+            Using useEffect and intervals, the pet's mood changes dynamically over time, becoming tired, stinky, and angry if neglected.
+            I hand-drew the pet myself and am still refining the design, UI, and features to enhance the gameplay experience.
+            PixelPet will be deployed soon!`,
+        },
+        {
+            id: 4,
+            title: "Frames & Levels",
+            image: frames,
+            description: `Frames & Levels is a MERN-stack gaming platform where users curate their game collections, track owned and wishlist titles, and share reviews and thoughts on each game. 
+            The platform helps gamers rediscover old favorites and explore new adventures.
+            Built as a Single Page Application (SPA) with React, it features a REST API backend using ExpressJS, MongoDB, and Mongoose, supporting full CRUD functionality across multiple database models. 
+            Users can sign up, log in, and manage their collections securely with encrypted authentication.`,
+            link: "https://frames-and-levels.netlify.app/"
         }
     ];
     return (
