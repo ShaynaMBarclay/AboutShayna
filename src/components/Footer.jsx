394d71db4d903490a5d8e7824dc75a94e7cffedb
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-04-01 12:25:10 -0400

    fixed sizing on larger monitors

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+ const Footer = () => {
+  return (
+    <>
+    <footer className="footer">
+     <div className="footer-box">
+        <div className="footer-links">
+            <a href="/">
+            Home
+            </a>
+        </div>
+     </div>
+    </footer>
+   </>
+  )
+}
+
+export default Footer;
