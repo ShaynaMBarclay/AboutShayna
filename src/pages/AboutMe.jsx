1f93a65a79b081ab8b72905f3370cca1fd185aa8
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-05-15 11:41:11 -0400

    changed about me layout so it looks nicer on mobile

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -3,19 +3,10 @@ import { FaLightbulb, FaSyncAlt, FaHardHat, FaRocket } from "react-icons/fa";
 
 
 function AboutMe() {
-    
     return (
         <div>
             <section className="about-me">
-                <div className="about-text">
-             <p>Hello! I'm a Full-Stack Web Developer based in the United States, passionate about turning ideas into dynamic, real-world solutions. < br />
-              My creative background has shaped my ability to approach coding with an intuitive sense of design, balance, and innovation. < br /> 
-              I earned a certificate from Ironhack France, excelling in Frontend and Backend development while mastering remote collaboration across time zones. < br /> 
-              My love for travel and diverse cultures has also taught me to embrace flexibility and teamwork. Let’s work together to bring your vision to life!
-                </p>  
-                </div>
-                <div className="divider"></div>
-             <div className="skills-section">
+                  <div className="skills-section">
                     <div className="skills-boxes">
                         <div className="skill-box">
                         <FaLightbulb className="skill-icon" />
@@ -38,6 +29,17 @@ function AboutMe() {
                    
                     </div>
                 </div>
+            
+                <div className="divider"></div>
+                    <div className="about-text">
+             <p>Hello! I'm a Full-Stack Web Developer based in the United States, passionate about turning ideas into dynamic, real-world solutions. < br />
+              My creative background has shaped my ability to approach coding with an intuitive sense of design, balance, and innovation. < br /> 
+              I earned a certificate from Ironhack France, excelling in Frontend and Backend development while mastering remote collaboration across time zones. < br /> 
+              My love for travel and diverse cultures has also taught me to embrace flexibility and teamwork. Let’s work together to bring your vision to life!
+                </p>  
+                </div>
+
+           
             </section>
         </div>
         
