90e9a1c8e47bea0395feb0727103e4640d42d1e5
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-06-12 11:43:52 -0400

    updated resume

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import pfp from '../assets/pfpblacknwhite.jpg';
-import resume from "../assets/resumeSHAYNABARCLAYUpdated.pdf";
+import resume from "../assets/SBResume.pdf";
 
 function HomePage() {
     return (
